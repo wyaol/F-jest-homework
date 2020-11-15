601db41a47fc54937ff974a7609d0187ed39a650
wyao <289672494@qq.com>
2020-11-15 21:44:37 +0800

    [wang yao] feature: todo14-16

--- src/mock.class/__test__/vaccineTest.test.js
+++ src/mock.class/__test__/vaccineTest.test.js
@@ -1,27 +1,39 @@
 import VaccineTest from "../vaccineTest";
-import Recipient from "../recipient";
 import Covid19Vaccine from "../covid19Vaccine";
 
-jest.mock("../recipient", () => {
-  // mock class实现
-});
+const mockFun = jest.fn(() => true);
+const mockFun2 = jest.fn();
+
+jest.mock("../recipient", () =>
+  jest.fn(() => {
+    return {
+      getHasAntibodies: mockFun,
+      acceptInjection: mockFun2,
+    };
+  })
+);
 
 beforeEach(() => {
-  // clear mock here
+  jest.resetModules();
 });
 
 describe("inject", () => {
   test("should recipient accept injection with vaccine", () => {
-    // TODO 14: add test here
+    const vaccineTest = new VaccineTest();
+    vaccineTest.inject();
+    expect(mockFun2).toHaveBeenCalledWith(expect.any(Covid19Vaccine));
   });
 });
 
 describe("test", () => {
   test("should get Success if recipient has antibodies", () => {
-    // TODO 15: add test here
+    const vaccineTest = new VaccineTest();
+    expect(vaccineTest.test()).toEqual("Vaccine Test Success");
   });
 
   test("should get Failed if recipient has no antibodies", () => {
-    // TODO 16: add test here
+    const vaccineTest = new VaccineTest();
+    mockFun.mockImplementation(() => false);
+    expect(vaccineTest.test()).toEqual("Vaccine Test Failed");
   });
 });
